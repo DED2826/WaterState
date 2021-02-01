87f80ea0239f56a9f9bf67dae44b857e483d7f2e
Ded2826 <sasadedenko28@gmail.com>
2021-02-01 19:05:40 +0200

    Add water state

---
+++ Pattern State/Pattern State/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pattern_State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Water water = new Water(WaterState.LIQUID);
+            water.Heat();
+            water.Heat();
+            water.Frost();
+            water.Frost();
+            water.Frost();
+            water.Heat();
+
+
+
+            Console.Read();
+        }
+    }
+}
